2506643db7ed850510da391cecbf13b899d602d6
andres.delikat <andres.delikat@57e5bc84-f80e-11dd-a7be-4b3ef3b0700c>
2009-06-03 02:20:25 +0000

    Frontend - misc fix ups

--- win32/Mednafen-Front/Mednafen-Front/Front.vb
+++ win32/Mednafen-Front/Mednafen-Front/Front.vb
@@ -1,11 +1,15 @@
-﻿Public Class Form1
-    'Globals
+﻿Public Class Front
+    'Constants
     Dim MEDNAFEN As String = "mednafen.exe "
     Dim MOVIE As String = "-mov "
+    Friend EMUVERSION As String = "Mednafen Rerecording 1.1"
+    Friend FRONTVERSION As String = "Frontend 1.0"
+
+    'Globals
     Dim CommandLine As String = ""      'Command line to run
 
     Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-
+        Me.Text = EMUVERSION + " " + FRONTVERSION
     End Sub
 
     Private Sub ExitToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ExitToolStripMenuItem.Click
@@ -75,5 +79,9 @@
         CommandBox.Text = "\"""
 
         CommandBox.Text = MEDNAFEN + movieCommand + """" + MovieBox.Text() + """" + " " + OtherCommands.Text() + " " + """" + RomBox.Text() + """"
+    End Sub
+
+    Private Sub AboutToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles AboutToolStripMenuItem.Click
+        About.ShowDialog()
     End Sub
 End Class
