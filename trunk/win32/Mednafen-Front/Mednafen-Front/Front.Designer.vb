1409c8b3f48d8f1b95d2488250020914b694a4dc
andres.delikat <andres.delikat@57e5bc84-f80e-11dd-a7be-4b3ef3b0700c>
2009-06-03 02:20:25 +0000

    Frontend - misc fix ups

--- trunk/win32/Mednafen-Front/Mednafen-Front/Front.Designer.vb
+++ trunk/win32/Mednafen-Front/Mednafen-Front/Front.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class Front
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -26,6 +26,7 @@ Partial Class Form1
         Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
         Me.RomToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
         Me.MovieToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator
         Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
         Me.OptionsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
         Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
@@ -44,8 +45,9 @@ Partial Class Form1
         Me.Label4 = New System.Windows.Forms.Label
         Me.RomBrowse = New System.Windows.Forms.Button
         Me.MovieBrowse = New System.Windows.Forms.Button
-        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox
         Me.MenuStrip1.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
         '
         'MenuStrip1
@@ -53,7 +55,7 @@ Partial Class Form1
         Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.OptionsToolStripMenuItem, Me.HelpToolStripMenuItem})
         Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
         Me.MenuStrip1.Name = "MenuStrip1"
-        Me.MenuStrip1.Size = New System.Drawing.Size(477, 24)
+        Me.MenuStrip1.Size = New System.Drawing.Size(511, 24)
         Me.MenuStrip1.TabIndex = 0
         Me.MenuStrip1.Text = "MenuStrip1"
         '
@@ -67,19 +69,24 @@ Partial Class Form1
         'RomToolStripMenuItem
         '
         Me.RomToolStripMenuItem.Name = "RomToolStripMenuItem"
-        Me.RomToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.RomToolStripMenuItem.Size = New System.Drawing.Size(125, 22)
         Me.RomToolStripMenuItem.Text = "&Rom..."
         '
         'MovieToolStripMenuItem
         '
         Me.MovieToolStripMenuItem.Name = "MovieToolStripMenuItem"
-        Me.MovieToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.MovieToolStripMenuItem.Size = New System.Drawing.Size(125, 22)
         Me.MovieToolStripMenuItem.Text = "&Movie..."
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(122, 6)
         '
         'ExitToolStripMenuItem
         '
         Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
-        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(113, 22)
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(125, 22)
         Me.ExitToolStripMenuItem.Text = "E&xit"
         '
         'OptionsToolStripMenuItem
@@ -104,7 +111,7 @@ Partial Class Form1
         'AboutToolStripMenuItem
         '
         Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
-        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(114, 22)
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
         Me.AboutToolStripMenuItem.Text = "&About"
         '
         'LaunchButton
@@ -119,7 +126,7 @@ Partial Class Form1
         'ExitButton
         '
         Me.ExitButton.DialogResult = System.Windows.Forms.DialogResult.Cancel
-        Me.ExitButton.Location = New System.Drawing.Point(322, 215)
+        Me.ExitButton.Location = New System.Drawing.Point(369, 215)
         Me.ExitButton.Name = "ExitButton"
         Me.ExitButton.Size = New System.Drawing.Size(75, 23)
         Me.ExitButton.TabIndex = 2
@@ -130,22 +137,22 @@ Partial Class Form1
         '
         Me.OtherCommands.Location = New System.Drawing.Point(28, 169)
         Me.OtherCommands.Name = "OtherCommands"
-        Me.OtherCommands.Size = New System.Drawing.Size(382, 20)
+        Me.OtherCommands.Size = New System.Drawing.Size(416, 20)
         Me.OtherCommands.TabIndex = 3
         '
         'CommandBox
         '
-        Me.CommandBox.Location = New System.Drawing.Point(25, 309)
+        Me.CommandBox.Location = New System.Drawing.Point(33, 309)
         Me.CommandBox.Name = "CommandBox"
         Me.CommandBox.ReadOnly = True
-        Me.CommandBox.Size = New System.Drawing.Size(372, 20)
+        Me.CommandBox.Size = New System.Drawing.Size(411, 20)
         Me.CommandBox.TabIndex = 4
         Me.CommandBox.Text = "mednafen.exe"
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(22, 280)
+        Me.Label1.Location = New System.Drawing.Point(30, 280)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(80, 13)
         Me.Label1.TabIndex = 5
@@ -162,7 +169,7 @@ Partial Class Form1
         '
         'Update
         '
-        Me.Update.Location = New System.Drawing.Point(322, 280)
+        Me.Update.Location = New System.Drawing.Point(344, 22)
         Me.Update.Name = "Update"
         Me.Update.Size = New System.Drawing.Size(75, 23)
         Me.Update.TabIndex = 7
@@ -173,14 +180,14 @@ Partial Class Form1
         '
         Me.MovieBox.Location = New System.Drawing.Point(28, 110)
         Me.MovieBox.Name = "MovieBox"
-        Me.MovieBox.Size = New System.Drawing.Size(301, 20)
+        Me.MovieBox.Size = New System.Drawing.Size(335, 20)
         Me.MovieBox.TabIndex = 8
         '
         'RomBox
         '
         Me.RomBox.Location = New System.Drawing.Point(28, 61)
         Me.RomBox.Name = "RomBox"
-        Me.RomBox.Size = New System.Drawing.Size(301, 20)
+        Me.RomBox.Size = New System.Drawing.Size(335, 20)
         Me.RomBox.TabIndex = 9
         '
         'Label3
@@ -203,7 +210,7 @@ Partial Class Form1
         '
         'RomBrowse
         '
-        Me.RomBrowse.Location = New System.Drawing.Point(335, 59)
+        Me.RomBrowse.Location = New System.Drawing.Point(369, 59)
         Me.RomBrowse.Name = "RomBrowse"
         Me.RomBrowse.Size = New System.Drawing.Size(75, 23)
         Me.RomBrowse.TabIndex = 12
@@ -212,32 +219,36 @@ Partial Class Form1
         '
         'MovieBrowse
         '
-        Me.MovieBrowse.Location = New System.Drawing.Point(335, 106)
+        Me.MovieBrowse.Location = New System.Drawing.Point(369, 106)
         Me.MovieBrowse.Name = "MovieBrowse"
         Me.MovieBrowse.Size = New System.Drawing.Size(75, 23)
         Me.MovieBrowse.TabIndex = 13
         Me.MovieBrowse.Text = "Browse..."
         Me.MovieBrowse.UseVisualStyleBackColor = True
         '
-        'ToolStripSeparator1
+        'GroupBox1
         '
-        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
-        Me.ToolStripSeparator1.Size = New System.Drawing.Size(149, 6)
+        Me.GroupBox1.Controls.Add(Me.Update)
+        Me.GroupBox1.Location = New System.Drawing.Point(25, 258)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(474, 90)
+        Me.GroupBox1.TabIndex = 14
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Debug"
         '
-        'Form1
+        'Front
         '
         Me.AcceptButton = Me.LaunchButton
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.CancelButton = Me.ExitButton
-        Me.ClientSize = New System.Drawing.Size(477, 350)
+        Me.ClientSize = New System.Drawing.Size(511, 350)
         Me.Controls.Add(Me.MovieBrowse)
         Me.Controls.Add(Me.RomBrowse)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.RomBox)
         Me.Controls.Add(Me.MovieBox)
-        Me.Controls.Add(Me.Update)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.CommandBox)
@@ -245,11 +256,13 @@ Partial Class Form1
         Me.Controls.Add(Me.ExitButton)
         Me.Controls.Add(Me.LaunchButton)
         Me.Controls.Add(Me.MenuStrip1)
+        Me.Controls.Add(Me.GroupBox1)
         Me.MainMenuStrip = Me.MenuStrip1
-        Me.Name = "Form1"
-        Me.Text = "Mednafen rerecording"
+        Me.Name = "Front"
+        Me.Text = "Mednafen rerecording Frontend"
         Me.MenuStrip1.ResumeLayout(False)
         Me.MenuStrip1.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -277,5 +290,6 @@ Partial Class Form1
     Friend WithEvents RomBrowse As System.Windows.Forms.Button
     Friend WithEvents MovieBrowse As System.Windows.Forms.Button
     Friend WithEvents ToolStripSeparator1 As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
 
 End Class
