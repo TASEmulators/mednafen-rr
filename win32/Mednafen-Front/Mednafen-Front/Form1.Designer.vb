55730e57c6f5f6702dd308a597fd39ce9f304d6b
andres.delikat <andres.delikat@57e5bc84-f80e-11dd-a7be-4b3ef3b0700c>
2009-06-03 01:43:58 +0000

    Added a mednafen-rerecording front end project

---
+++ win32/Mednafen-Front/Mednafen-Front/Form1.Designer.vb
@@ -0,0 +1,281 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.RomToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.MovieToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.OptionsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.HelpToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.LaunchButton = New System.Windows.Forms.Button
+        Me.ExitButton = New System.Windows.Forms.Button
+        Me.OtherCommands = New System.Windows.Forms.TextBox
+        Me.CommandBox = New System.Windows.Forms.TextBox
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.Update = New System.Windows.Forms.Button
+        Me.MovieBox = New System.Windows.Forms.TextBox
+        Me.RomBox = New System.Windows.Forms.TextBox
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.Label4 = New System.Windows.Forms.Label
+        Me.RomBrowse = New System.Windows.Forms.Button
+        Me.MovieBrowse = New System.Windows.Forms.Button
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.OptionsToolStripMenuItem, Me.HelpToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(477, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.RomToolStripMenuItem, Me.MovieToolStripMenuItem, Me.ToolStripSeparator1, Me.ExitToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(35, 20)
+        Me.FileToolStripMenuItem.Text = "&File"
+        '
+        'RomToolStripMenuItem
+        '
+        Me.RomToolStripMenuItem.Name = "RomToolStripMenuItem"
+        Me.RomToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.RomToolStripMenuItem.Text = "&Rom..."
+        '
+        'MovieToolStripMenuItem
+        '
+        Me.MovieToolStripMenuItem.Name = "MovieToolStripMenuItem"
+        Me.MovieToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.MovieToolStripMenuItem.Text = "&Movie..."
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(113, 22)
+        Me.ExitToolStripMenuItem.Text = "E&xit"
+        '
+        'OptionsToolStripMenuItem
+        '
+        Me.OptionsToolStripMenuItem.Name = "OptionsToolStripMenuItem"
+        Me.OptionsToolStripMenuItem.Size = New System.Drawing.Size(56, 20)
+        Me.OptionsToolStripMenuItem.Text = "&Options"
+        '
+        'HelpToolStripMenuItem
+        '
+        Me.HelpToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.HelpToolStripMenuItem1, Me.AboutToolStripMenuItem})
+        Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
+        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(40, 20)
+        Me.HelpToolStripMenuItem.Text = "&Help"
+        '
+        'HelpToolStripMenuItem1
+        '
+        Me.HelpToolStripMenuItem1.Name = "HelpToolStripMenuItem1"
+        Me.HelpToolStripMenuItem1.Size = New System.Drawing.Size(114, 22)
+        Me.HelpToolStripMenuItem1.Text = "&Help"
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(114, 22)
+        Me.AboutToolStripMenuItem.Text = "&About"
+        '
+        'LaunchButton
+        '
+        Me.LaunchButton.Location = New System.Drawing.Point(28, 215)
+        Me.LaunchButton.Name = "LaunchButton"
+        Me.LaunchButton.Size = New System.Drawing.Size(75, 23)
+        Me.LaunchButton.TabIndex = 1
+        Me.LaunchButton.Text = "Launch!"
+        Me.LaunchButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.ExitButton.Location = New System.Drawing.Point(322, 215)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(75, 23)
+        Me.ExitButton.TabIndex = 2
+        Me.ExitButton.Text = "E&xit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'OtherCommands
+        '
+        Me.OtherCommands.Location = New System.Drawing.Point(28, 169)
+        Me.OtherCommands.Name = "OtherCommands"
+        Me.OtherCommands.Size = New System.Drawing.Size(382, 20)
+        Me.OtherCommands.TabIndex = 3
+        '
+        'CommandBox
+        '
+        Me.CommandBox.Location = New System.Drawing.Point(25, 309)
+        Me.CommandBox.Name = "CommandBox"
+        Me.CommandBox.ReadOnly = True
+        Me.CommandBox.Size = New System.Drawing.Size(372, 20)
+        Me.CommandBox.TabIndex = 4
+        Me.CommandBox.Text = "mednafen.exe"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(22, 280)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(80, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Command Line:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(25, 150)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(87, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Other commands"
+        '
+        'Update
+        '
+        Me.Update.Location = New System.Drawing.Point(322, 280)
+        Me.Update.Name = "Update"
+        Me.Update.Size = New System.Drawing.Size(75, 23)
+        Me.Update.TabIndex = 7
+        Me.Update.Text = "Update"
+        Me.Update.UseVisualStyleBackColor = True
+        '
+        'MovieBox
+        '
+        Me.MovieBox.Location = New System.Drawing.Point(28, 110)
+        Me.MovieBox.Name = "MovieBox"
+        Me.MovieBox.Size = New System.Drawing.Size(301, 20)
+        Me.MovieBox.TabIndex = 8
+        '
+        'RomBox
+        '
+        Me.RomBox.Location = New System.Drawing.Point(28, 61)
+        Me.RomBox.Name = "RomBox"
+        Me.RomBox.Size = New System.Drawing.Size(301, 20)
+        Me.RomBox.TabIndex = 9
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(28, 45)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(32, 13)
+        Me.Label3.TabIndex = 10
+        Me.Label3.Text = "ROM"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(25, 94)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(36, 13)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "Movie"
+        '
+        'RomBrowse
+        '
+        Me.RomBrowse.Location = New System.Drawing.Point(335, 59)
+        Me.RomBrowse.Name = "RomBrowse"
+        Me.RomBrowse.Size = New System.Drawing.Size(75, 23)
+        Me.RomBrowse.TabIndex = 12
+        Me.RomBrowse.Text = "Browse..."
+        Me.RomBrowse.UseVisualStyleBackColor = True
+        '
+        'MovieBrowse
+        '
+        Me.MovieBrowse.Location = New System.Drawing.Point(335, 106)
+        Me.MovieBrowse.Name = "MovieBrowse"
+        Me.MovieBrowse.Size = New System.Drawing.Size(75, 23)
+        Me.MovieBrowse.TabIndex = 13
+        Me.MovieBrowse.Text = "Browse..."
+        Me.MovieBrowse.UseVisualStyleBackColor = True
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(149, 6)
+        '
+        'Form1
+        '
+        Me.AcceptButton = Me.LaunchButton
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.ExitButton
+        Me.ClientSize = New System.Drawing.Size(477, 350)
+        Me.Controls.Add(Me.MovieBrowse)
+        Me.Controls.Add(Me.RomBrowse)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.RomBox)
+        Me.Controls.Add(Me.MovieBox)
+        Me.Controls.Add(Me.Update)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.CommandBox)
+        Me.Controls.Add(Me.OtherCommands)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.LaunchButton)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Mednafen rerecording"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents FileToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents RomToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents MovieToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents OptionsToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents HelpToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents HelpToolStripMenuItem1 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents AboutToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents LaunchButton As System.Windows.Forms.Button
+    Friend WithEvents ExitButton As System.Windows.Forms.Button
+    Friend WithEvents OtherCommands As System.Windows.Forms.TextBox
+    Friend WithEvents CommandBox As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Update As System.Windows.Forms.Button
+    Friend WithEvents MovieBox As System.Windows.Forms.TextBox
+    Friend WithEvents RomBox As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents RomBrowse As System.Windows.Forms.Button
+    Friend WithEvents MovieBrowse As System.Windows.Forms.Button
+    Friend WithEvents ToolStripSeparator1 As System.Windows.Forms.ToolStripSeparator
+
+End Class
---
+++ win32/Mednafen-Front/Mednafen-Front/Form1.vb
@@ -0,0 +1,79 @@
+﻿Public Class Form1
+    'Globals
+    Dim MEDNAFEN As String = "mednafen.exe "
+    Dim MOVIE As String = "-mov "
+    Dim CommandLine As String = ""      'Command line to run
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ExitToolStripMenuItem.Click
+        'Exit
+        CloseIt()
+    End Sub
+
+    Private Sub ExitButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ExitButton.Click
+        'Exit
+        CloseIt()
+    End Sub
+
+    Private Sub Update_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Update.Click
+        UpdateCommandLine()
+    End Sub
+
+    Private Sub LaunchButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles LaunchButton.Click
+        UpdateCommandLine()
+        CommandLine = CommandBox.Text()
+        ''Launch mednafen using CommandLine
+        Shell(CommandBox.Text(), AppWinStyle.NormalFocus, False, -1)
+    End Sub
+
+    Private Sub RomBrowse_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles RomBrowse.Click
+        Dim OpenRom As New OpenFileDialog
+        OpenRom.InitialDirectory = System.Environment.CurrentDirectory
+        OpenRom.Filter = "Supported Files|*.nes;*.unf;*.fds;*.pce;*.lnx;*.o;*.ngp;*.ngc;*.ws;*.wsc|" & _
+            "NES (*.nes, *.unf, *.fds)|*.nes;*.unf;*.fds|" & _
+            "PCE (*.pce)|*.pce|" & _
+            "Lynx (*.lnx, *.o)|*.lnx;*.o|" & _
+            "NGP (*.ngp, .ngc)|*.ngp;*.ngc|" & _
+            "WS (*.ws, *.wsc)|*.ws;*.wsc|" & _
+            "All files (*.*)|*.*"
+        OpenRom.RestoreDirectory = False
+
+
+        OpenRom.ShowDialog()
+        If (OpenRom.FileName.Length) Then
+            RomBox.Text = OpenRom.FileName
+        End If
+    End Sub
+
+    Private Sub MovieBrowse_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MovieBrowse.Click
+        Dim OpenMovie As New SaveFileDialog
+        OpenMovie.InitialDirectory = System.Environment.CurrentDirectory
+        OpenMovie.Filter = "Mednafen movie file (*.mcm)|*.mcm"
+        OpenMovie.RestoreDirectory = False
+
+
+        OpenMovie.ShowDialog()
+        If (OpenMovie.FileName.Length) Then
+            MovieBox.Text = OpenMovie.FileName
+        End If
+
+    End Sub
+
+    'Subs
+    Private Sub CloseIt()
+        Me.Close()
+    End Sub
+
+    Private Sub UpdateCommandLine()
+        Dim movieCommand As String = ""
+        If (MovieBox.Text.Length()) Then
+            movieCommand = MOVIE
+        End If
+        CommandBox.Text = "\"""
+
+        CommandBox.Text = MEDNAFEN + movieCommand + """" + MovieBox.Text() + """" + " " + OtherCommands.Text() + " " + """" + RomBox.Text() + """"
+    End Sub
+End Class
