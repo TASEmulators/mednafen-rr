4680a631532507cfaadeecec8bfa27a59cf15723
andres.delikat <andres.delikat@57e5bc84-f80e-11dd-a7be-4b3ef3b0700c>
2009-06-07 02:05:53 +0000

    Frontend - If user closed mednafen, pressing the close mednafen button will cause the frontend to realize this and correct itself.

--- trunk/win32/Mednafen-Front/Mednafen-Front/About.Designer.vb
+++ trunk/win32/Mednafen-Front/Mednafen-Front/About.Designer.vb
@@ -23,19 +23,27 @@ Partial Class About
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox
+        Me.Label2 = New System.Windows.Forms.Label
         Me.GroupBox2 = New System.Windows.Forms.GroupBox
-        Me.GroupBox3 = New System.Windows.Forms.GroupBox
-        Me.Label1 = New System.Windows.Forms.Label
-        Me.FrontVersionLabel = New System.Windows.Forms.Label
-        Me.EmuVersionLabel = New System.Windows.Forms.Label
         Me.Label4 = New System.Windows.Forms.Label
+        Me.EmuVersionLabel = New System.Windows.Forms.Label
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox
+        Me.FrontVersionLabel = New System.Windows.Forms.Label
+        Me.Label1 = New System.Windows.Forms.Label
         Me.OkButton = New System.Windows.Forms.Button
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.Label5 = New System.Windows.Forms.Label
+        Me.Label6 = New System.Windows.Forms.Label
+        Me.GroupBox1.SuspendLayout()
         Me.GroupBox2.SuspendLayout()
         Me.GroupBox3.SuspendLayout()
         Me.SuspendLayout()
         '
         'GroupBox1
         '
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
         Me.GroupBox1.Location = New System.Drawing.Point(34, 39)
         Me.GroupBox1.Name = "GroupBox1"
         Me.GroupBox1.Size = New System.Drawing.Size(200, 85)
@@ -43,8 +51,18 @@ Partial Class About
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "Mednafen"
         '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(9, 64)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(58, 13)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "2005-2009"
+        '
         'GroupBox2
         '
+        Me.GroupBox2.Controls.Add(Me.Label5)
         Me.GroupBox2.Controls.Add(Me.Label4)
         Me.GroupBox2.Controls.Add(Me.EmuVersionLabel)
         Me.GroupBox2.Location = New System.Drawing.Point(34, 145)
@@ -53,6 +71,24 @@ Partial Class About
         Me.GroupBox2.TabIndex = 1
         Me.GroupBox2.TabStop = False
         Me.GroupBox2.Text = "Mednafen rerecording"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(9, 44)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(166, 13)
+        Me.Label4.TabIndex = 1
+        Me.Label4.Text = "Authors: paul_t, adelikat, zeromus"
+        '
+        'EmuVersionLabel
+        '
+        Me.EmuVersionLabel.AutoSize = True
+        Me.EmuVersionLabel.Location = New System.Drawing.Point(9, 24)
+        Me.EmuVersionLabel.Name = "EmuVersionLabel"
+        Me.EmuVersionLabel.Size = New System.Drawing.Size(60, 13)
+        Me.EmuVersionLabel.TabIndex = 0
+        Me.EmuVersionLabel.Text = "Version 1.1"
         '
         'GroupBox3
         '
@@ -65,6 +101,15 @@ Partial Class About
         Me.GroupBox3.TabStop = False
         Me.GroupBox3.Text = "Mednafen Frontend"
         '
+        'FrontVersionLabel
+        '
+        Me.FrontVersionLabel.AutoSize = True
+        Me.FrontVersionLabel.Location = New System.Drawing.Point(9, 21)
+        Me.FrontVersionLabel.Name = "FrontVersionLabel"
+        Me.FrontVersionLabel.Size = New System.Drawing.Size(63, 13)
+        Me.FrontVersionLabel.TabIndex = 1
+        Me.FrontVersionLabel.Text = "Version: 1.0"
+        '
         'Label1
         '
         Me.Label1.AutoSize = True
@@ -74,33 +119,6 @@ Partial Class About
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Author: adelikat"
         '
-        'FrontVersionLabel
-        '
-        Me.FrontVersionLabel.AutoSize = True
-        Me.FrontVersionLabel.Location = New System.Drawing.Point(9, 20)
-        Me.FrontVersionLabel.Name = "FrontVersionLabel"
-        Me.FrontVersionLabel.Size = New System.Drawing.Size(63, 13)
-        Me.FrontVersionLabel.TabIndex = 1
-        Me.FrontVersionLabel.Text = "Version: 1.0"
-        '
-        'EmuVersionLabel
-        '
-        Me.EmuVersionLabel.AutoSize = True
-        Me.EmuVersionLabel.Location = New System.Drawing.Point(9, 26)
-        Me.EmuVersionLabel.Name = "EmuVersionLabel"
-        Me.EmuVersionLabel.Size = New System.Drawing.Size(60, 13)
-        Me.EmuVersionLabel.TabIndex = 0
-        Me.EmuVersionLabel.Text = "Version 1.1"
-        '
-        'Label4
-        '
-        Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(9, 48)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(166, 13)
-        Me.Label4.TabIndex = 1
-        Me.Label4.Text = "Authors: paul_t, adelikat, zeromus"
-        '
         'OkButton
         '
         Me.OkButton.Location = New System.Drawing.Point(159, 340)
@@ -109,6 +127,34 @@ Partial Class About
         Me.OkButton.TabIndex = 3
         Me.OkButton.Text = "Ok"
         Me.OkButton.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(9, 42)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(107, 13)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "The Mednafen Team"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(9, 65)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(79, 13)
+        Me.Label5.TabIndex = 2
+        Me.Label5.Text = "Released 2009"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(9, 21)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(70, 13)
+        Me.Label6.TabIndex = 2
+        Me.Label6.Tag = "Version 0.8.B"
+        Me.Label6.Text = "Version 0.8.B"
         '
         'About
         '
@@ -121,6 +167,8 @@ Partial Class About
         Me.Controls.Add(Me.GroupBox1)
         Me.Name = "About"
         Me.Text = "Mednafen Rerecording"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
         Me.GroupBox2.ResumeLayout(False)
         Me.GroupBox2.PerformLayout()
         Me.GroupBox3.ResumeLayout(False)
@@ -136,4 +184,8 @@ Partial Class About
     Friend WithEvents FrontVersionLabel As System.Windows.Forms.Label
     Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents OkButton As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
 End Class
