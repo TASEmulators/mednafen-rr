2eb800038a4053ba2fbc609bf815b5afc05f7e99
andres.delikat <andres.delikat@57e5bc84-f80e-11dd-a7be-4b3ef3b0700c>
2009-06-08 23:19:06 +0000

    Frontend-forgot to commit these files.

---
+++ win32/Mednafen-Front/Mednafen-Front/Help.Designer.vb
@@ -0,0 +1,140 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Help
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.LinkLabel2 = New System.Windows.Forms.LinkLabel
+        Me.Label4 = New System.Windows.Forms.Label
+        Me.LinkLabel3 = New System.Windows.Forms.LinkLabel
+        Me.OK = New System.Windows.Forms.Button
+        Me.SuspendLayout()
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(63, 63)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(197, 13)
+        Me.LinkLabel1.TabIndex = 1
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Tag = ""
+        Me.LinkLabel1.Text = "http://code.google.com/p/mednafen-rr/"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(75, 25)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(172, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Madenafen rerecording Homepage"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(77, 40)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(169, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "and Commandline Documentation:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(63, 106)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(197, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Additional Commandline Documentation:"
+        '
+        'LinkLabel2
+        '
+        Me.LinkLabel2.AutoSize = True
+        Me.LinkLabel2.Location = New System.Drawing.Point(18, 129)
+        Me.LinkLabel2.Name = "LinkLabel2"
+        Me.LinkLabel2.Size = New System.Drawing.Size(287, 13)
+        Me.LinkLabel2.TabIndex = 5
+        Me.LinkLabel2.TabStop = True
+        Me.LinkLabel2.Text = "http://mednafen.sourceforge.net/documentation/#using-cli"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(82, 168)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(158, 13)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Mednafen-Front documentation:"
+        '
+        'LinkLabel3
+        '
+        Me.LinkLabel3.AutoSize = True
+        Me.LinkLabel3.Location = New System.Drawing.Point(9, 191)
+        Me.LinkLabel3.Name = "LinkLabel3"
+        Me.LinkLabel3.Size = New System.Drawing.Size(304, 13)
+        Me.LinkLabel3.TabIndex = 7
+        Me.LinkLabel3.TabStop = True
+        Me.LinkLabel3.Text = "http://code.google.com/p/mednafen-rr/w/edit/MednafenFront"
+        '
+        'OK
+        '
+        Me.OK.Location = New System.Drawing.Point(102, 231)
+        Me.OK.Name = "OK"
+        Me.OK.Size = New System.Drawing.Size(75, 23)
+        Me.OK.TabIndex = 8
+        Me.OK.Text = "&Ok"
+        Me.OK.UseVisualStyleBackColor = True
+        '
+        'Help
+        '
+        Me.AcceptButton = Me.OK
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.OK
+        Me.ClientSize = New System.Drawing.Size(322, 273)
+        Me.Controls.Add(Me.OK)
+        Me.Controls.Add(Me.LinkLabel3)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.LinkLabel2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Name = "Help"
+        Me.Text = "Mednafen-Front Help"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents LinkLabel1 As System.Windows.Forms.LinkLabel
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents LinkLabel2 As System.Windows.Forms.LinkLabel
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents LinkLabel3 As System.Windows.Forms.LinkLabel
+    Friend WithEvents OK As System.Windows.Forms.Button
+End Class
---
+++ win32/Mednafen-Front/Mednafen-Front/Help.vb
@@ -0,0 +1,20 @@
+﻿Public Class Help
+    Private Sub OK_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OK.Click
+        Me.Hide()
+    End Sub
+
+    Private Sub LinkLabel1_LinkClicked(ByVal sender As System.Object, ByVal e As System.Windows.Forms.LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        Me.LinkLabel1.LinkVisited = True
+        System.Diagnostics.Process.Start(LinkLabel1.Text())
+    End Sub
+
+    Private Sub LinkLabel2_LinkClicked(ByVal sender As System.Object, ByVal e As System.Windows.Forms.LinkLabelLinkClickedEventArgs) Handles LinkLabel2.LinkClicked
+        Me.LinkLabel2.LinkVisited = True
+        System.Diagnostics.Process.Start(LinkLabel2.Text())
+    End Sub
+
+    Private Sub LinkLabel3_LinkClicked(ByVal sender As System.Object, ByVal e As System.Windows.Forms.LinkLabelLinkClickedEventArgs) Handles LinkLabel3.LinkClicked
+        Me.LinkLabel3.LinkVisited = True
+        System.Diagnostics.Process.Start(LinkLabel3.Text())
+    End Sub
+End Class
